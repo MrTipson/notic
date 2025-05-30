be8b689f8dfc4603d56fe7f37bcac5ee1e3e5f95
MrTipson <mr@tipson.xyz>
2025-05-30 23:35:04 +0200

    Vite: fix @ imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mdx from '@mdx-js/rollup';
 import tailwindcss from "@tailwindcss/vite";
+import path from 'node:path';
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -13,7 +14,11 @@ export default defineConfig(async () => ({
     tailwindcss(),
     react()
   ],
-
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
