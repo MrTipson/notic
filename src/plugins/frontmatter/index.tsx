5cf3e7fe830ad223ddbdc0a7d35268a0fd017c42
MrTipson <mr@tipson.xyz>
2025-06-01 17:45:44 +0200

    Plugins/frontmatter: resolve relative paths found in frontmatter

--- src/plugins/frontmatter/index.tsx
+++ src/plugins/frontmatter/index.tsx
@@ -4,11 +4,23 @@ import remarkFrontmatter from 'https://esm.sh/remark-frontmatter';
 import remarkMdxFrontmatter from 'https://esm.sh/remark-mdx-frontmatter';
 import { NoticPlugin } from '..';
 
-export const {title, description, options}: NoticPlugin = {
+export const {title, description, options, apply}: NoticPlugin = {
     title: 'Frontmatter',
     description: 'Parse frontmatter in the header of markdown files and export it a variable\
 that can be used in the body or by external files.',
     options: {
         remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter]
+    },
+    apply: async ({ filename, props }) => {
+        // resolve relative paths
+        if (!!filename && !!props?.frontmatter) {
+            const dirpath = filename.substring(0, filename.lastIndexOf('/'));
+            const fm = props.frontmatter as {[index: string]: unknown};
+            for (const k in fm) {
+                if (typeof fm[k] === 'string' && (fm[k].startsWith('./') || fm[k].startsWith('../'))) {
+                    fm[k] = dirpath + '/' + fm[k];
+                }
+            }
+        }
     },
 }
