e1a5e33a63ebf0a9c8bfe0897e322fbdca3b9970
MrTipson <mr@tipson.xyz>
2025-06-01 19:34:12 +0200

    Plugins/app-layout: improve divider look and feel

--- src/plugins/app-layout/Split.tsx
+++ src/plugins/app-layout/Split.tsx
@@ -8,7 +8,7 @@ interface SplitProps {
 }
 export function Split(props: SplitProps) {
     const { type, a, b } = props;
-    const [ratio, setRatio] = useState(0.3);
+    const [ratio, setRatio] = useState(0.4);
     const [drag, setDrag] = useState(false);
     const me = useRef<HTMLDivElement>(null);
 
@@ -23,17 +23,23 @@ export function Split(props: SplitProps) {
         }
     }, [drag, setRatio]);
     const onMouseUp: MouseEventHandler = useCallback(event => {
-        if (event.button === 0) {
+        if (!drag) return;
+        if (event.type === 'mouseup' && event.button === 0 || event.type === 'mouseleave') {
             setDrag(false);
+            // Magnetise edges a bit
+            if (ratio < 0.05) setRatio(0);
+            if (ratio > 0.95) setRatio(1);
         }
-    }, [setRatio]);
+    }, [ratio, drag]);
 
     const flex = type === 'horizontal' ? 'flex' : 'flex flex-col';
     return (
-        <div ref={me} className={'w-full h-full ' + flex} onMouseMove={onMouseMove} onMouseUp={onMouseUp}>
+        <div ref={me} className={'w-full h-full ' + flex} onMouseMove={onMouseMove} onMouseUp={onMouseUp} onMouseLeave={onMouseUp}>
             <div className='shrink-0 min-w-0 min-h-0' style={{ flexBasis: ratio*100 + '%' }}>{a}</div>
-            <Divider {...{drag, setDrag, type}} />
-            <div className={'grow ' + (type === 'vertical' ? 'overflow-auto' : '')}>{b}</div>
+            <div className='grow relative overflow-visible min-h-0'>
+                <div className={'h-full w-full relative ' + (type === 'vertical' ? 'overflow-auto' : '')}>{b}</div>
+                <Divider {...{drag, setDrag, type}} />
+            </div>
         </div>
     );
 }
@@ -53,10 +59,10 @@ function Divider(props: DividerProps) {
         }
     }, [setDrag]);
 
-    let className = 'hover:bg-c1-accent shrink-0 ' +
-        (type === 'horizontal' ? 'h-full w-2' : 'w-full h-2');
+    let className = 'shrink-0 absolute p-4 from-transparent from-25% hover:via-c1-accent/50 to-75% to-transparent cursor-grab ' +
+        (type === 'horizontal' ? 'h-full w-2 left-0 translate-x-[-50%] top-0 bg-gradient-to-r' : 'w-full h-2 top-0 translate-y-[-50%] bg-gradient-to-b');
     if (drag) {
-        className += ' bg-c1-accent';
+        className += ' via-c1-accent/50 cursor-grabbing';
     }
     return <div {...{className, onMouseDown}}/>;
 }
